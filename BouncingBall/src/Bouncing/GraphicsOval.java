1de39308b067083420f5144502336da22fc101d3
Mamunahmed33 <mamunahmed33@gmail.com>
2016-09-30 01:03:10 +0600

    BouncingBall repo is uploaded in git. But need refactor and to work on in

---
+++ BouncingBall/src/Bouncing/GraphicsOval.java
@@ -0,0 +1,157 @@
+package Bouncing;
+/** Developer: Khandaker Mamun Ahmed 
+ * 	Date: 11-04-2014
+ */
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+import java.util.Vector;
+
+import javax.swing.JPanel;
+
+public class GraphicsOval extends JPanel implements Runnable{
+	private int width = 30, height = 20;
+//	private GraphicsOvalMain graphicsOvalMain = new GraphicsOvalMain();
+
+	private Vector<Integer> vecX= new Vector<Integer>();									// X position
+	private Vector<Integer> vecY= new Vector<Integer>();									// Y position
+	private Vector<Integer> vecBooleanX= new Vector<Integer>();							//check wheather x will need to increment(for 0) or decrement(for 1)
+	private Vector<Integer> vecBooleanY= new Vector<Integer>();							//check wheather Y will need to increment(for 0) or decrement(for 1)
+	
+	private int timeSpeedUp = 0;
+	private int timer = 20;
+	private boolean generateBall;
+	private boolean exit = true;
+	private int xPos;
+	private int setScore= 0;
+	private Random randomGenerator = new Random();
+
+	public synchronized void  paintComponent(Graphics g){
+		super.paintComponent(g);
+		
+		if(generateBall == false)
+			for(int i = 0; i<10; i++){
+				vecX.add(randomGenerator.nextInt(480));
+				vecY.add(randomGenerator.nextInt(400));
+				vecBooleanX.add(i % 2);
+				vecBooleanY.add(0);
+				
+				// Not to generate ball all at once
+				try {
+					Thread.sleep(20);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println(vecBooleanX.get(i) +"  "+ vecX.get(i));	
+			}
+		
+		for(int i = 0; i< vecX.size(); i++){
+			g.setColor(Color.BLUE);
+			g.fillOval(vecX.get(i), vecY.get(i), width, height);
+		}
+			this.generateBall = true;
+			
+			exitCheck();
+	}
+	
+	public void run() {
+		while(exit){
+			
+			timeSpeedUp++;
+			this.checkValidation();
+			
+			System.out.println(vecBooleanY.size()-1);
+			
+			for(int i= 0 ; i <= vecY.size() - 1; i++){
+				if(vecBooleanX.get(i) == 0)
+					vecX.set(i, vecX.get(i)+1);
+				else if(vecBooleanX.get(i) == 1)
+					vecX.set(i, vecX.get(i)-1);
+				
+				if(vecBooleanY.get(i) == 0)
+					vecY.set(i, vecY.get(i)+1);
+				else if(vecBooleanY.get(i) == 1)
+					vecY.set(i, vecY.get(i)-1);
+			}
+			
+			try {
+				Thread.sleep(timer);
+				
+				if(timeSpeedUp%200 ==0 && timer > 1){
+					timer--; timeSpeedUp=0;
+				}
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			repaint();
+		}
+	}
+
+	
+	private synchronized void checkValidation(){								// sets the x and y position boolean
+		for(int i = 0; i<= vecX.size()-1; i++){
+			
+			if(vecY.get(i) == 0){
+				vecBooleanY.set(i, 0);
+				vecY.set(i, vecY.get(i)+3);
+				System.out.println("Hiii");
+			}
+			else if(vecX.get(i) >= (xPos-20) && vecX.get(i) <=  xPos+150 && vecY.get(i) == (480-(height+10))){
+				vecBooleanY.set(i, 1);    System.out.println("OOOW");
+				
+				setScore++;
+			//	graphicsOvalMain.setCounter(setScore);
+				GraphicsOvalMain.scoreCounter.setText(""+setScore);
+			}
+			
+			if(vecX.get(i) == (480 -width)){
+				vecBooleanX.set(i, 1);
+				System.out.println("harder");
+			}
+			
+			else if(vecX.get(i) == 0)
+				vecBooleanX.set(i, 0);
+			
+			if(vecX.get(i) <  xPos-20 && vecY.get(i) == 480){
+				
+				System.out.println( vecX.get(i)+ "  Hi  "+ new GraphicsOvalMain().getxPos()+ "   "+vecY.get(i) + "  " +((480-(height+10))));
+				vecX.remove(i);
+				vecY.remove(i);
+				vecBooleanX.remove(i);
+				vecBooleanY.remove(i);
+			}
+			
+			else if(vecX.get(i) > (xPos+150) && vecY.get(i) == 480)
+			{
+				System.out.println( vecX.get(i)+ "  Hello  "+ (xPos +150)+  "   " +vecY.get(i) + "  " +((480-height)));
+				vecX.remove(i);
+				vecY.remove(i);
+				vecBooleanX.remove(i);
+				vecBooleanY.remove(i);
+			}
+		}
+	}
+	
+	public void setMotionSpeed(int value) {
+		timer = value;
+	}
+	
+	public void ballSize(int width, int height){
+		this.width = width;
+		this.height = height;
+	}
+	
+	public void setXpos(int x){
+		this.xPos = x;
+	}
+	
+	private void exitCheck(){									// checks when to exit the program
+		if(vecX.isEmpty())
+			exit = false;
+	}
+}
+
---
+++ BouncingBall/src/Bouncing/GraphicsOvalMain.java
@@ -0,0 +1,253 @@
+package Bouncing;
+/** Developer: Khandaker Mamun Ahmed 
+ * 	Date: 11-04-2014
+ */
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/*import org.jdesktop.swingx.painter.AbstractLayoutPainter.HorizontalAlignment;
+*/
+import com.jtattoo.plaf.acryl.AcrylLookAndFeel;
+import com.jtattoo.plaf.aluminium.AluminiumLookAndFeel;
+import com.jtattoo.plaf.bernstein.BernsteinLookAndFeel;
+import com.jtattoo.plaf.fast.FastLookAndFeel;
+import com.jtattoo.plaf.graphite.GraphiteLookAndFeel;
+import com.jtattoo.plaf.luna.LunaLookAndFeel;
+import com.jtattoo.plaf.mcwin.McWinLookAndFeel;
+import com.jtattoo.plaf.mint.MintLookAndFeel;
+import com.jtattoo.plaf.noire.NoireLookAndFeel;
+import com.jtattoo.plaf.smart.SmartLookAndFeel;
+import com.jtattoo.plaf.texture.TextureLookAndFeel;
+
+
+public class GraphicsOvalMain extends JFrame{
+	//private JPanel contentPane;
+	private static GraphicsOval gh;
+	private JLabel stant, timer, ballSize, dxBall, score;
+	//private JTextField scoreCounter;
+	private JSlider timeSlider, ballSizeSlider;
+	private int xPos= 150;
+	private JButton leftButton, rightButton;
+	private static int scoreCount = 0;
+	
+	
+	public static	JTextField scoreCounter = new JTextField(""+ scoreCount);
+    JPanel	contentPane = new JPanel();
+	GraphicsOvalMain(){
+		scoreCounter.setText(""+ scoreCounter);
+	}
+	
+	public void setCounter(int score){
+		
+	}
+	
+	public void initCOmponent(){
+		setTitle("Bouncing Ball");
+		
+		ImageIcon icon = new ImageIcon(getClass().getResource("icon.jpg"));  
+        setIconImage(icon.getImage());  
+		//contentPane = new JPanel();
+		contentPane.setLayout(null);
+		//contentPane.setBackground(Color.lightGray);
+		
+		gh = new GraphicsOval();
+		gh.setLayout(null);
+		gh.setBounds(0, 0, 480, 480);
+		gh.addMouseMotionListener(new MyMouseAdapter());
+		contentPane.add(gh);
+		
+		dxBall = new JLabel("**DX BALL**");
+		dxBall.setBounds(500, 20, 170, 30);
+		dxBall.setFont(new java.awt.Font("SansSerif", 1, 28));
+		//dxBall.setForeground(new Color(180, 50, 200));
+		contentPane.add(dxBall);
+		
+		stant = new JLabel();
+		stant.setIcon(new ImageIcon(getClass().getResource("Stant.JPG")));
+		stant.setBounds(xPos , 460, 150, 30);
+		stant.addMouseMotionListener(new MyMouseAdapter());
+		gh.add(stant);
+		gh.setXpos(xPos);
+		
+		timer = new JLabel("Speed");
+		timer.setBounds(530, 100, 100, 30);
+		timer.setFont(new java.awt.Font("SansSerif", 1, 24));
+		timer.setForeground(new Color(180, 50, 200));
+		contentPane.add(timer);
+		
+		timeSlider = new JSlider(SwingConstants.HORIZONTAL, 0, 200, 0);
+		timeSlider.setBounds(530, 130, 80, 15);
+		timeSlider.setMinorTickSpacing(2);
+		timeSlider.setMajorTickSpacing(5);
+		timeSlider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				if(timeSlider.getValue() < 1)
+					gh.setMotionSpeed(1);
+				else if(timeSlider.getValue() > 250)
+					gh.setMotionSpeed(250);
+				else
+					gh.setMotionSpeed(timeSlider.getValue());
+			}});
+		contentPane.add(timeSlider);
+		
+		ballSize = new JLabel("Ball Size");
+		ballSize.setBounds(530, 150, 100, 30);
+		ballSize.setFont(new java.awt.Font("SansSerif", 1, 24));
+		ballSize.setForeground(new Color(180, 50, 200));
+		contentPane.add(ballSize);
+		
+		ballSizeSlider = new JSlider(SwingConstants.HORIZONTAL, 0, 200, 0);
+		ballSizeSlider.setBounds(530, 180, 50, 15);
+		ballSizeSlider.setMajorTickSpacing(1);
+		ballSizeSlider.setMinorTickSpacing((int) .5);
+		ballSizeSlider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				if(ballSizeSlider.getValue() < 15)
+					gh.ballSize(15, 10);
+				else if(ballSizeSlider.getValue() > 70)
+					gh.ballSize(70, 55);
+				else
+					gh.ballSize(ballSizeSlider.getValue(), (ballSizeSlider.getValue()*2)/ 3);
+			}});
+		contentPane.add(ballSizeSlider);
+
+		leftButton = new JButton();
+		leftButton.setIcon(new ImageIcon(getClass().getResource("left.JPG")));
+		leftButton.setBounds(530, 220, 50, 30);
+		leftButton.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent e) {
+				if(xPos < 0 )
+					xPos = 0;
+				else
+					xPos -= 10;
+				gh.setXpos(xPos);
+				stant.setBounds(xPos , 460, 150, 30);
+			}
+		});
+		contentPane.add(leftButton);
+
+		rightButton = new JButton();
+		rightButton.setIcon(new ImageIcon(getClass().getResource("Right.JPG")));
+		rightButton.setBounds(580, 220, 50, 30);
+		rightButton.addActionListener(new ActionListener(){
+			//@Override
+			public void actionPerformed(ActionEvent e) {
+				if(xPos > 330)
+					xPos = 330;
+				else
+					xPos += 10;
+				gh.setXpos(xPos);
+				stant.setBounds(xPos , 460, 150, 30);
+			}
+		});
+		contentPane.add(rightButton);
+		
+		score = new JLabel("Score");
+		score.setFont(new java.awt.Font("SansSerif", 1, 24));
+		score.setForeground(new Color(180, 50, 200));
+		score.setBounds(530, 280, 70, 30);
+		contentPane.add(score);
+		
+		//scoreCounter.setHorizontalAlignment(JTextField.CENTER);
+		scoreCounter.setEditable(false);
+		//scoreCounter.setFont(new java.awt.Font("SansSerif", 1, 24));
+		scoreCounter.setForeground(new Color(180, 50, 200));
+		scoreCounter.setBounds(540, 320, 50, 30);
+		contentPane.add(scoreCounter);
+		
+		setContentPane(contentPane);
+		setVisible(true);
+		setResizable(false);
+		setSize(700, 520);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	class MyMouseAdapter extends MouseAdapter {
+		   
+		   public void mouseMoved(MouseEvent e) {
+			   xPos = e.getX();
+			   
+			   //scoreCounter.setText(""+ scoreCount);
+				
+			   System.out.println("\t\t\t\t"+xPos);
+			   gh.setXpos(e.getX());
+			     stant.setBounds(xPos , 460, 150, 30);
+		   }
+		
+		
+		@Override
+		   public void mouseDragged(MouseEvent e) {
+		     xPos = e.getX();
+		     if(xPos< 0)
+		    	 xPos = 0;
+		     else if(xPos> 400)
+		    	 xPos = 250;
+		     stant.setBounds(xPos , 460, 150, 30);
+		   }
+
+	}
+	
+	public int getxPos() {
+		return xPos;
+	}
+
+//	public void setCounter(int score){
+//		this.scoreCount = score;
+//		System.out.println(scoreCount+"            :)");		
+//	}
+
+	
+	
+	public static void main(String[] args) {
+		try {
+		//	UIManager.setLookAndFeel(new BernsteinLookAndFeel());
+		//	UIManager.setLookAndFeel(new AcrylLookAndFeel());
+		//	UIManager.setLookAndFeel(new AluminiumLookAndFeel());
+		//	UIManager.setLookAndFeel(new FastLookAndFeel());
+		//	UIManager.setLookAndFeel(new GraphiteLookAndFeel());
+		//	UIManager.setLookAndFeel(new LunaLookAndFeel());
+		//	UIManager.setLookAndFeel(new McWinLookAndFeel());
+		//	UIManager.setLookAndFeel(new MintLookAndFeel());
+			UIManager.setLookAndFeel(new NoireLookAndFeel());
+		//	UIManager.setLookAndFeel(new SmartLookAndFeel());
+		//	UIManager.setLookAndFeel(new TextureLookAndFeel());
+			//UIManager.setLookAndFeel("com.jtattoo.plaf.smart.SmartLookAndFeel");
+			
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+		new GraphicsOvalMain().initCOmponent();
+		
+		ExecutorService e1 = Executors.newCachedThreadPool();
+		//ExecutorService e2 = Executors.newCachedThreadPool();
+		
+		
+		e1.execute(gh);
+		//e2.execute(new Timer());
+		
+		e1.shutdown();
+		//e2.shutdown();
+	}
+}
